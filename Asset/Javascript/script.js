0acc6dd029201ba6f30e34633bfeb29ad4fbae9a
Ey_jun <gregoiresenechal@outlook.com>
2024-03-22 14:07:14 +0100

    Version 1.0 finished

--- Asset/Javascript/script.js
+++ Asset/Javascript/script.js
@@ -1,7 +1,7 @@
 const board = document.getElementById("board");
 let forme;
 let isPlayer1 = true;
-let isfinish = true;
+let isFinish = false;
 const win_combinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -14,36 +14,36 @@ const win_combinations = [
 ];
 
 const gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const para = document.querySelector(".p1").value;
+const para2 = document.querySelector(".p2").value;
 
 const endMessage = document.createElement("h2");
-endMessage.textContent = `Player1's turn!`;
+endMessage.textContent = `Lemon's turn!`;
 endMessage.style.marginTop = "30px";
 endMessage.style.textAlign = "center";
 board.after(endMessage);
 
-const para = document.querySelector(".p1");
-const para2 = document.querySelector(".p2");
+// para.addEventListener("click", updateName);
+// para2.addEventListener("click", updateName2);
 
-para.addEventListener("click", updateName);
-para2.addEventListener("click", updateName2);
+// function updateName() {
+//   const name = prompt("Enter a new name");
+//   para.textContent = `Player 1: ${name}`;
+// }
 
-function updateName() {
-  const name = prompt("Enter a new name");
-  para.textContent = `Player 1: ${name}`;
-}
-
-function updateName2() {
-  const name = prompt("Enter a new name");
-  para2.textContent = `Player 2: ${name}`;
-}
+// function updateName2() {
+//   const name = prompt("Enter a new name");
+//   para2.textContent = `Player 2: ${name}`;
+// }
 
 class Forme {
-  constructor(width, height, positionX, positionY, color) {
+  constructor(width, height, positionX, positionY, color, radius) {
     this.width = width;
     this.height = height;
     this.positionX = positionX;
     this.positionY = positionY;
     this.color = color;
+    this.radius = radius;
   }
 
   display() {
@@ -51,14 +51,14 @@ class Forme {
     forme.style.width = `${this.width}px`;
     forme.style.height = `${this.height}px`;
     forme.style.backgroundColor = this.color;
-    forme.style.borderTopLeftRadius = "25px solid transparent";
+    forme.style.borderTopLeftRadius = `30px`;
+    forme.style.borderBottomRightRadius = `30px`;
   }
 }
 
 class Circle extends Forme {
   constructor(width, height, positionX, positionY, color, radius) {
     super(width, height, positionX, positionY, color, radius);
-    this.radius = radius;
   }
 
   display() {
@@ -70,54 +70,61 @@ class Circle extends Forme {
 let squares = document.querySelectorAll(".square");
 squares.forEach((square) => {
   square.addEventListener("click", (e) => {
+    toRename(e, square);
+  });
+});
+
+function toRename(e, square) {
+  if (!isFinish) {
     let number = parseInt(square.id.substring(6), 10);
     if (gameBoard[number] == 0) {
       if (isPlayer1 === true) {
-        let cube = new Forme(50, 50, e.clientX, e.clientY, "purple");
+        let cube = new Forme(50, 50, e.clientX, e.clientY, "yellow", "5px");
         cube.display();
         square.appendChild(forme);
 
         gameBoard[number] = 1;
         if (checkWinCombination() == false) {
           isPlayer1 = false;
-          endMessage.innerText = `Player 2's turn!`;
+          endMessage.innerText = `Orange's turn!`;
         }
       } else {
-        let circle = new Circle(50, 50, e.clientX, e.clientY, "blue", "50px");
+        let circle = new Circle(50, 50, e.clientX, e.clientY, "orange", "50px");
         circle.display();
         square.appendChild(forme);
 
         gameBoard[number] = 2;
         if (checkWinCombination() == false) {
           isPlayer1 = true;
-          endMessage.innerText = `Player 1's turn!`;
+          endMessage.innerText = `Lemon's turn!`;
         }
       }
     } else {
-      alert("Cette case est déjà cochée");
+      alert("This box is already checked");
     }
 
     function checkWinCombination() {
-      console.log(gameBoard);
       for (let combi of win_combinations) {
-        console.log(gameBoard[combi[0]], gameBoard[combi[1]]); // combi = [0,1,0]
+        // combi = [0,1,0]
         if (
           gameBoard[combi[0]] == gameBoard[combi[1]] &&
           gameBoard[combi[0]] == gameBoard[combi[2]] &&
           gameBoard[combi[0]] != 0
         ) {
           if (gameBoard[combi[0]] == 1) {
-            endMessage.innerText = "Players 1 Won!";
+            endMessage.innerText = `Lemon Won!`;
+            endGame();
           } else if (gameBoard[combi[0]] == 2) {
-            endMessage.innerText = "Players 2 Won !";
+            endMessage.innerText = `Orange Won!`;
+            endGame();
           }
           return true;
         }
       }
       return false;
     }
-  });
-});
+  }
+}
 
 const input = document.getElementById("restartButton");
 input.addEventListener("click", restartButton);
@@ -126,9 +133,19 @@ function restartButton() {
   for (let i = 0; i < squares.length; i++) {
     squares[i].textContent = "";
   }
-  endMessage.textContent = `Player1's turn!`;
+  endMessage.textContent = `Lemon's turn!`;
   for (let number = 0; number < gameBoard.length; number++) {
     gameBoard[number] = 0;
   }
   isPlayer1 = true;
+  isFinish = false;
+}
+
+function endGame() {
+  if (
+    endMessage.innerText == "Lemon Won!" ||
+    endMessage.innerText == "Orange Won!"
+  ) {
+    isFinish = true;
+  }
 }
